bab393b70f22df6838667e32fe7ed63b970f7ffe
Emma Brown <emma.b@comcast.net>
2025-08-03 13:45:23 -0400

    Initial Commit

---
+++ Bee Helper/Bee_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bee_HelperApp.swift
+//  Bee Helper
+//
+//  Created by Emma Brown on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Bee_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bee HelperTests/Bee_HelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bee_HelperTests.swift
+//  Bee HelperTests
+//
+//  Created by Emma Brown on 8/3/25.
+//
+
+import Testing
+@testable import Bee_Helper
+
+struct Bee_HelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
