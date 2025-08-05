4a0961a57a1eb21510d2c50f9b4ff062b9b0a8ff
Emma Brown <emma.b@comcast.net>
2025-08-04 21:57:48 -0400

    Restore VIEW PUZZLE button and green test button functionality

--- Bee Helper.xcodeproj/Bee Helper/Views/ArchivePuzzleDetailView.swift
+++ Bee Helper.xcodeproj/Bee Helper/Views/ArchivePuzzleDetailView.swift
@@ -149,25 +149,7 @@ struct ArchivePuzzleDetailView: View {
     }
 }
 
-struct StatCard: View {
-    let title: String
-    let value: String
-    let color: Color
-    
-    var body: some View {
-        VStack(spacing: 8) {
-            Text(value)
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundColor(color)
-            
-            Text(title)
-                .font(.caption)
-                .foregroundColor(.secondary)
-                .multilineTextAlignment(.center)
-        }
-    }
-}
+
 
 #Preview {
     let samplePuzzle = PuzzleData(
