05638dab2fb4de850f5625111f99e62fac8e74a8
Emma Brown <emma.b@comcast.net>
2025-08-05 22:30:56 -0400

    Clean up: Remove duplicate files from wrong location

--- Bee Helper.xcodeproj/add_files_to_xcode.sh
+++ Bee Helper.xcodeproj/add_files_to_xcode.sh
@@ -1,1 +1,10 @@
- 
+#!/bin/bash
+
+# Add new files to Xcode project
+echo "Adding DictionaryService.swift to Xcode project..."
+xcodebuild -project "Bee Helper.xcodeproj" -target "Bee Helper" -add-file "Bee Helper/Services/DictionaryService.swift"
+
+echo "Adding NetworkStatusView.swift to Xcode project..."
+xcodebuild -project "Bee Helper.xcodeproj" -target "Bee Helper" -add-file "Bee Helper/Views/NetworkStatusView.swift"
+
+echo "Files added to Xcode project!" 
